3e2f413b62ace7c30ca18e8f99e2ea02b496242c
Sch <junhg0211@gmail.com>
2024-07-28 22:14:06 +0900

    default ephemeral

--- cogs/util.py
+++ cogs/util.py
@@ -11,11 +11,11 @@ diacritics = {
 
 class UtilCog(Cog):
     @command(name="다이어크리틱", description="문자열에 다이어크리틱을 붙입니다.")
-    @describe(string="변경할 문자열")
-    async def search(self, ctx: Interaction, string: str):
+    @describe(string="변경할 문자열", ephemeral="결과 비공개 여부")
+    async def search(self, ctx: Interaction, string: str, ephemeral: bool = True):
         for key, value in sorted(diacritics.items(), key=lambda x: len(x[0]), reverse=True):
             string = string.replace(key, value)
-        await ctx.response.send_message(f'```\n{string}```')
+        await ctx.response.send_message(f'```\n{string}```', ephemeral=ephemeral)
 
 
 async def setup(bot: Bot):
